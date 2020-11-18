aa76c42476066ab11e0696f4be10cb2e42e44db7
zhousg <zhoushugang@itcast.cn>
2020-11-18 18:16:06 +0800

    首页-底部

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,20 +1,25 @@
 <template>
   <AppTopnav/>
   <AppHeader/>
-  <main>
+  <main class="app-body">
     <!-- 二级路由 -->
     <router-view></router-view>
   </main>
-  <footer>底部</footer>
+  <AppFooter/>
 </template>
 
 <script>
 import AppTopnav from '@/components/app-topnav'
 import AppHeader from '@/components/app-header'
+import AppFooter from '@/components/app-footer'
 export default {
   name: 'XtxLayout',
-  components: { AppTopnav, AppHeader }
+  components: { AppTopnav, AppHeader, AppFooter }
 }
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.app-body {
+  min-height: 666px;
+}
+</style>
