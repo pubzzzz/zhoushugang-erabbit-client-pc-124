ce2ed0df6a1c88991d25d069f696ca7239053237
zhousg <zhoushugang@itcast.cn>
2020-11-30 10:02:59 +0800

    面板-骨架效果

--- src/views/home/components/home-new.vue
+++ src/views/home/components/home-new.vue
@@ -1,24 +1,30 @@
 <template>
   <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
     <template v-slot:right><XtxMore /></template>
-    <ul ref="pannel" class="goods-list">
-      <li v-for="item in goods" :key="item.id">
-        <RouterLink to="/">
-          <img :src="item.picture" alt="">
-          <p class="name">{{item.name}}</p>
-          <p class="price">&yen;{{item.price}}</p>
-        </RouterLink>
-      </li>
-    </ul>
+    <div style="position: relative;height: 406px;">
+      <Transition name="fade">
+        <ul v-if="goods.length" ref="pannel" class="goods-list">
+          <li v-for="item in goods" :key="item.id">
+            <RouterLink to="/">
+              <img :src="item.picture" alt="">
+              <p class="name">{{item.name}}</p>
+              <p class="price">&yen;{{item.price}}</p>
+            </RouterLink>
+          </li>
+        </ul>
+        <HomeSkeleton bg="#f0f9f4" v-else />
+      </Transition>
+    </div>
   </HomePanel>
 </template>
 
 <script>
 import HomePanel from './home-panel'
+import HomeSkeleton from './home-skeleton'
 import { findNew } from '@/api/home'
 export default {
   name: 'HomeNew',
-  components: { HomePanel },
+  components: { HomePanel, HomeSkeleton },
   data () {
     return {
       goods: []
