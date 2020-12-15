bccdbb6c097930be88d6fa70b9ba5c11e6b9e0c4
zhousg <zhoushugang@itcast.cn>
2020-12-15 17:56:47 +0800

    登录-表单绘制

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,6 +6,58 @@
         <a @click="activeName='account'" :class="{active:activeName==='account'}" href="javascript:;">账户登录</a>
         <a @click="activeName='qrcode'" :class="{active:activeName==='qrcode'}" href="javascript:;">扫码登录</a>
       </nav>
+      <div class="account-box" v-if="activeName==='account'">
+        <div class="toggle">
+          <a @click="isMsgLogin=false" href="javascript:;" v-if="isMsgLogin">
+            <i class="iconfont icon-user"></i> 使用账号登录
+          </a>
+          <a @click="isMsgLogin=true" href="javascript:;" v-else>
+            <i class="iconfont icon-msg"></i> 使用短信登录
+          </a>
+        </div>
+        <div class="form">
+          <div class="form-item">
+            <div class="input">
+              <i class="iconfont icon-user"></i>
+              <input type="text" :placeholder="isMsgLogin?'请输入手机号':'请输入账号'">
+            </div>
+            <!-- <div class="error"><i class="iconfont icon-warning" />请输入手机号</div> -->
+          </div>
+          <div class="form-item" v-if="!isMsgLogin">
+            <div class="input">
+              <i class="iconfont icon-lock"></i>
+              <input type="password" placeholder="请输入密码">
+            </div>
+          </div>
+          <div class="form-item" v-else>
+            <div class="input">
+              <i class="iconfont icon-code"></i>
+              <input type="password" placeholder="请输入验证码">
+              <span class="code">发送验证码</span>
+            </div>
+          </div>
+          <div class="form-item">
+            <div class="agree">
+              <span>我已同意</span>
+              <a href="javascript:;">《隐私条款》</a>
+              <span>和</span>
+              <a href="javascript:;">《服务条款》</a>
+            </div>
+          </div>
+          <a href="javascript:;" class="btn">登录</a>
+        </div>
+        <div class="action">
+          <img src="https://qzonestyle.gtimg.cn/qzone/vas/opensns/res/img/Connect_logo_7.png" alt="">
+          <div class="url">
+            <a href="javascript:;">忘记密码</a>
+            <a href="javascript:;">免费注册</a>
+          </div>
+        </div>
+      </div>
+      <div class="qrcode-box" v-if="activeName==='qrcode'">
+        <img src="@/assets/images/qrcode.jpg" alt="">
+        <p>打开 <a href="javascript:;">小兔鲜App</a> 扫码登录</p>
+      </div>
     </div>
   </section>
   <LoginFooter />
@@ -23,13 +75,128 @@ export default {
   data () {
     return {
       // account 账户登录 qrcode 扫码登录
-      activeName: 'account'
+      activeName: 'account',
+      // 是否短信登录
+      isMsgLogin: false
     }
   }
 }
 </script>
 
 <style scoped lang='less'>
+// 二维码容器
+.qrcode-box {
+  text-align: center;
+  padding-top: 40px;
+  p {
+    margin-top: 20px;
+    a {
+      color: @xtxColor;
+      font-size: 16px;
+    }
+  }
+}
+// 账号容器
+.account-box {
+  .toggle {
+    padding: 15px 40px;
+    text-align: right;
+    a {
+      color: @xtxColor;
+      i {
+        font-size: 14px;
+      }
+    }
+  }
+  .form {
+    padding: 0 40px;
+    &-item {
+      margin-bottom: 28px;
+      .input {
+        position: relative;
+        height: 36px;
+        i {
+          width: 34px;
+          height: 34px;
+          background: #cfcdcd;
+          color: #fff;
+          position: absolute;
+          left: 1px;
+          top: 1px;
+          text-align: center;
+          line-height: 34px;
+          font-size: 18px;
+        }
+        input {
+          padding-left: 44px;
+          border: 1px solid #cfcdcd;
+          height: 36px;
+          line-height: 36px;
+          width: 100%;
+          &.error {
+            border-color: @priceColor;
+          }
+          &.active,&:focus {
+            border-color: @xtxColor;
+          }
+        }
+        .code {
+          position: absolute;
+          right: 1px;
+          top: 1px;
+          text-align: center;
+          line-height: 34px;
+          font-size: 14px;
+          background: #f5f5f5;
+          color: #666;
+          width: 90px;
+          height: 34px;
+          cursor: pointer;
+        }
+      }
+      .error {
+        position: absolute;
+        font-size: 12px;
+        line-height: 28px;
+        color: @priceColor;
+        i {
+          font-size: 14px;
+          margin-right: 2px;
+        }
+      }
+    }
+    .agree {
+      a {
+        color: #069;
+      }
+    }
+    .btn {
+      display: block;
+      width: 100%;
+      height: 40px;
+      color: #fff;
+      text-align: center;
+      line-height: 40px;
+      background: @xtxColor;
+      &.disabled {
+        background: #cfcdcd;
+      }
+    }
+  }
+  .action {
+    padding: 20px 40px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .url {
+      a {
+        color: #999;
+        margin-left: 10px;
+      }
+    }
+  }
+}
+// 主体容器
 .login-section {
   background: url(../../assets/images/login-bg.png) no-repeat center / cover;
   height: 488px;
@@ -37,7 +204,7 @@ export default {
   .wrapper {
     width: 380px;
     background: #fff;
-    min-height: 380px;
+    min-height: 400px;
     position: absolute;
     left: 50%;
     top: 54px;
@@ -48,15 +215,17 @@ export default {
       border-bottom: 1px solid #f5f5f5;
       display: flex;
       align-items: center;
+      padding: 0 40px;
       a {
         flex: 1;
         line-height: 1;
         display: inline-block;
-        text-align: center;
         font-size: 18px;
         position: relative;
+        text-align: right;
         &:first-child {
-          border-right: 1px solid #f5f5f5
+          border-right: 1px solid #f5f5f5;
+          text-align: left;
         }
         &:hover,&.active{
           color: @xtxColor;
