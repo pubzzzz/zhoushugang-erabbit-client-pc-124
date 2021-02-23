7ea1840c417e38408c88c1b5ae453d1c1c218553
zhousg <zhoushugang@itcast.cn>
2021-02-23 17:05:26 +0800

    商品热榜组件

--- src/api/goods.js
+++ src/api/goods.js
@@ -16,3 +16,12 @@ export const findGoods = (id) => {
 export const findRelGoods = (id, limit = 16) => {
   return request('/goods/relevant', 'get', { id, limit })
 }
+
+/**
+ * 获取热榜商品
+ * @param {Number} type - 1代表24小时热销榜 2代表周热销榜 3代表总热销榜
+ * @param {Number} limit - 获取个数
+ */
+export const findHotGoods = (type, limit = 3) => {
+  return request('/goods/hot', 'get', { type, limit })
+}
--- src/views/goods/components/goods-hot.vue
+++ src/views/goods/components/goods-hot.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="goods-hot">
     <h3>{{title}}</h3>
+    <GoodsItem v-for="item in goodsList" :key="item.id" :goods="item"/>
   </div>
 </template>
 <script>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
+import GoodsItem from '../../category/components/goods-item'
+import { findHotGoods } from '@/api/goods'
 export default {
   name: 'GoodsHot',
   props: {
@@ -13,13 +16,52 @@ export default {
       default: 1
     }
   },
+  components: { GoodsItem },
   setup (props) {
+    // 处理标题
     const titleObj = { 1: '24小时热销榜', 2: '周热销榜', 3: '总热销榜' }
     const title = computed(() => {
       return titleObj[props.type]
     })
-    return { title }
+    // 商品列表
+    const goodsList = ref([])
+    findHotGoods(props.type).then(data => {
+      goodsList.value = data.result.map(item => {
+        item.tag = item.desc
+        return item
+      })
+    })
+    return { title, goodsList }
   }
 }
 </script>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.goods-hot {
+  h3 {
+    height: 70px;
+    background: @helpColor;
+    color: #fff;
+    font-size: 18px;
+    line-height: 70px;
+    padding-left: 25px;
+    margin-bottom: 10px;
+    font-weight: normal;
+  }
+  ::v-deep .goods-item {
+    background: #fff;
+    width: 100%;
+    margin-bottom: 10px;
+    img {
+      width: 200px;
+      height: 200px;
+    }
+    p {
+      margin: 0 10px;
+    }
+    &:hover {
+      transform: none;
+      box-shadow: none;
+    }
+  }
+}
+</style>
